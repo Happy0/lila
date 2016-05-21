ebee67f37fbeeb775615872d484c9c14536f2af6
Gordon Martin <gordon.hugh.martin@gmail.com>
2016-05-21 15:04:19 +0100

    Flesh out what 'addNotification' will do a wee bit

--- modules/notify/src/main/Api.scala
+++ modules/notify/src/main/Api.scala
@@ -1,10 +1,27 @@
 package lila.notify
 
 import lila.common.paginator.Paginator
+import lila.hub.actorApi.SendTo
 
-private[notify] final class Api() {
+private[notify] final class Api(bus: lila.common.Bus) {
 
   def getNotifications(userId: String) : Fu[Paginator[Notification]] = ???
 
-  def addNotification(userId: String, notification: Notification) = ???
+  def addNotification(userId: String, notification: Notification) = {
+    // Add to database
+
+    val numUnreadNotifications = 1
+    // Notify client of new notification
+    notifyUserIfLoggedIn(userId, numUnreadNotifications)
+  }
+
+  def notifyUserIfLoggedIn(userId: String, unreadNotifications: Int) = {
+    val notificationsEventKey = "newNotifications"
+    val notificationEvent = SendTo(userId, notificationsEventKey, unreadNotifications)
+
+    bus.publish(notificationEvent, 'Users)
+
+  }
+
+
 }
