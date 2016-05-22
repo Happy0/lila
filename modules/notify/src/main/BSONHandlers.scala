f5690d72b02907ca2c0451745df7a6fb2934a37e
Gordon Martin <gordon.hugh.martin@gmail.com>
2016-05-22 17:24:07 +0100

    Remove unecessary nesting from 'notification' mongodb representation.

--- modules/notify/src/main/BSONHandlers.scala
+++ modules/notify/src/main/BSONHandlers.scala
@@ -1,9 +1,12 @@
 package lila.notify
 
 import lila.db.{dsl, BSON}
+import lila.hub.actorApi.notify.MentionedInThread.{Category, Topic, MentionedBy}
+import lila.hub.actorApi.notify.Notification.{Notifies, NotificationRead}
 import lila.hub.actorApi.notify.{MentionedInThread, Notification, NotificationContent}
 import lila.db.BSON.{ Reader, Writer }
 import lila.db.dsl._
+import reactivemongo.bson.BSONDocument
 
 private object BSONHandlers {
 
@@ -12,26 +15,48 @@ private object BSONHandlers {
     private def writeNotificationType(notificationContent: NotificationContent) = {
       notificationContent match {
         case MentionedInThread(mentionedBy, topic, category) => "mention"
-
       }
     }
 
     private def writeNotificationContent(notificationContent: NotificationContent) = {
       notificationContent match {
         case MentionedInThread(mentionedBy, topic, category) =>
-          $doc("mentionedBy" -> mentionedBy.value, "topic" -> topic.value, "category" -> category.value)
+          $doc("type" -> writeNotificationType(notificationContent), "mentionedBy" -> mentionedBy.value,
+            "topic" -> topic.value, "category" -> category.value)
       }
     }
 
-    override def reads(reader: Reader): NotificationContent = ???
+    private def readMentionedNotification(reader: Reader) : MentionedInThread = {
+      val mentionedBy = MentionedBy(reader.str("mentionedBy"))
+      val topic = Topic(reader.str("topic"))
+      val category = Category(reader.str("category"))
+
+      MentionedInThread(mentionedBy, topic, category)
+    }
+
+    override def reads(reader: Reader): NotificationContent = {
+      val notificationType = reader.str("type")
+
+      notificationType match {
+        case "mention" => readMentionedNotification(reader)
+      }
+    }
 
     override def writes(writer: Writer, n: NotificationContent): dsl.Bdoc = {
-      $doc("type" -> writeNotificationType(n), "content" -> writeNotificationContent(n))
+      writeNotificationContent(n)
     }
   }
 
   implicit val NotificationBSONHandler = new BSON[Notification] {
-    override def reads(reader: Reader): Notification = ???
+    override def reads(reader: Reader): Notification = {
+      val id = reader.str("_id")
+      val created = reader.date("created")
+      val hasRead = NotificationRead(reader.bool("read"))
+      val notifies = Notifies(reader.str("notifies"))
+      val content = reader.get[NotificationContent]("content")
+
+      Notification(id, notifies, content, hasRead, created)
+    }
 
     override def writes(writer: Writer, n: Notification): dsl.Bdoc = $doc(
       "_id" -> n.id,
